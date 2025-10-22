e2c6e9d6abe4b5d7e26a94c1c80e157fcc59df76
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-10-22 05:06:23 +0100

    save

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import user from './component/PropsComponent'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <PropsComponent />
+        
+    </>
+  )
+}
+
+export default App
